80d1a9f9be3af161efac2b1f4551b6eb263f0e4e
Meir Sarbassov <sarbasov@gmail.com>
2025-04-18 20:23:23 +0500

    return isinstance(member, (ChatMemberOwner, ChatMemberAdministrator, ChatMemberMember, ChatMemberRestricted))

--- bot/common/auth_utils.py
+++ bot/common/auth_utils.py
@@ -1,9 +1,12 @@
 from aiogram import Bot
+from aiogram.types import ChatMemberMember, ChatMemberLeft, ChatMemberOwner, ChatMemberAdministrator, \
+    ChatMemberRestricted
+
 from config import ADMIN_GROUP_ID
 
 async def is_admin(bot: Bot, user_id: int) -> bool:
     try:
         member = await bot.get_chat_member(ADMIN_GROUP_ID, user_id)
-        return member is not None
+        return isinstance(member, (ChatMemberOwner, ChatMemberAdministrator, ChatMemberMember, ChatMemberRestricted))
     except Exception:
         return False
